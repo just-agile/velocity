e4895ab61d592f66ebf0982070c52052c601572a
ishaq shaik <ishaqshaik.1706@gmail.com>
2017-05-03 12:57:48 -0700

    merging into branch

---
+++ firstrecorded.java
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="selenium.base" href="http://107.170.213.234/" />
+<title>firstrecorded</title>
+</head>
+<body>
+<table cellpadding="1" cellspacing="1" border="1">
+<thead>
+<tr><td rowspan="1" colspan="3">firstrecorded</td></tr>
+</thead><tbody>
+<tr>
+	<td>open</td>
+	<td>/catalog/logoff.php</td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>css=#tdb3 &gt; span.ui-button-text</td>
+	<td></td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>name=email_address</td>
+	<td>ecalix@test.com</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>name=password</td>
+	<td>test123</td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>id=tdb5</td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>css=#tdb4 &gt; span.ui-button-text</td>
+	<td></td>
+</tr>
+
+</tbody></table>
+</body>
+</html>
